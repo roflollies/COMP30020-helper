6c37fc00d3b2e9cad62c35d679889388e280b9c8
Samuel Xu <roflollies@users.noreply.github.com>
2017-08-28 04:11:15 +1000

    Made some comment changes
    it looks nicer now

--- test_initial.py
+++ test_initial.py
@@ -1,4 +1,7 @@
-#python script to find best initial
+# Python script compiles project 1 with all possible initial starting values
+# and uses Armaan Mcleod's average.py to compile a list of average guesses
+# and the respective initial starting value.
+# Outputs to cases.csv
 
 import itertools
 import subprocess
@@ -11,16 +14,15 @@ for x in itertools.combinations(i,3):
 	with open('Proj1.hs', 'r') as file:
 		data = file.readlines()
 
-	# Change the index to the line number (-1) and new initial guess string
-	data[] = ""
+	# !!!! Change the index to the line number-1 (remember arrays start at 0 lol) 
+	# 		     and new initial guess string !!!! #
+	data[] = "[\"{0}\",\"{1}\",\"{2}\"]".format(x[0],x[1],x[2] # remember to fill in the rest of the line!
 	with open('Proj1.hs', 'w') as file:
 		file.writelines(data)
 
 	subprocess.run("ghc -O2 Proj1Test", check=True)
 
 	out = subprocess.getoutput("python average.py")
-
-	# Make a csv file called cases first!!!!!!!
 	with open("cases.csv", "a") as myfile:
 		myfile.write("{0}, {1}\n".format(x,out))
 
